c96adf80bef8b14716d220034ec4c03f3cfec82b
StepanidaK <steshakirillina@yandex.ru>
2024-10-06 11:07:43 +0900

    add new kata solutions

---
+++ 8kyu/grasshooperCreateTheRooms.js
@@ -0,0 +1,27 @@
+/* https://www.codewars.com/kata/56a29b237e9e997ff2000048
+
+You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. 
+There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+
+*/
+
+const rooms = {
+    room1: {
+        name: "room1",
+        description: "this is the room1",
+        completed: true
+    },
+
+    room2: {
+        name: "room2",
+        description: "this is the room2",
+        completed: false
+    },
+
+    room3: {
+        name: "room3",
+        description: "this is the room3",
+        completed: true
+    }
+}
+
